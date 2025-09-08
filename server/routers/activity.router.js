99f26067f905c55fdc579dde5098f8d41ff16af9
PhithLoveSandwich <664259020@webmail.npru.ac.th>
2025-09-08 13:46:46 +0700

    Fixing ActivityControllers

--- server/routers/activity.router.js
+++ server/routers/activity.router.js
@@ -1,19 +1,25 @@
+// Correct Router
 import activityControllers from "../controllers/activity.controller.js";
 import express from "express";
-
 const router = express.Router();
 
-// GET http://localhost:3000/api/v1/activity/
+// GET all activities
+// GET http://localhost:3000/api/v1/activity
 router.get("/", activityControllers.getAll);
+// POST create activity
 // GET http://localhost:3000/api/v1/activity/:id
 router.post("/", activityControllers.createActivity);
+// GET activity by ID
 // GET http://localhost:3000/api/v1/activity/:id
 router.get("/:id", activityControllers.getById);
+// PUT update activity by ID
 // GET http://localhost:3000/api/v1/activity/:id
 router.put("/:id", activityControllers.updateActivity);
+// DELETE activity by ID
 // GET http://localhost:3000/api/v1/activity/:id
 router.delete("/:id", activityControllers.deleteActivity);
+// GET search activities
 // GET http://localhost:3000/api/v1/activity/:id
-router.get("/:id", activityControllers.serchActivities);
+router.get("/search", activityControllers.searchActivities);
 
 export default router;
