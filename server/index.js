2727a2a0d65c031a2b5b6e3f1bef48c0dfb8d9f6
PhithLoveSandwich <664259020@webmail.npru.ac.th>
2025-09-29 13:39:26 +0700

    ปรับแก้รัน Deploy docker compose ขึ้นทุกอันแล้ว

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
 app.use(cors({
-  origin: ["http://localhost:5174", "http://127.0.0.1:5174", FRONTEND_URL],
+  origin: ["http://localhost:5173", "http://127.0.0.1:5173", FRONTEND_URL],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization", "x-access-token"],
 }));
